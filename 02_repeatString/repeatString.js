0dcd0d28ecc385b0bbcce77924060cf86c0c3f27
Gui Costa <gui.costalp@gmail.com>
2022-06-07 23:43:51 +1200

    Java exercises. Ex2 missing final test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,40 @@
-const repeatString = function() {
+
+const repeatString = function(str, multi) {
+    multi = -5;
+    str = 'hey';
+
+    let multiStr = '';
+
+    if (multi < 0) {
+
+        return 'ERROR';
+    }
+    
+    else {
+
+        return multiStr = str.repeat(multi) ;
+    
+    }
+
+}
+
+
+
+
+/*
+const repeatString = function(str, multi) {
+    multi = 0;
+    str = 'hey';
+
+    let multiStr = '';
+
+    return multiStr = str.repeat(multi) ;
+    
 
 };
 
-// Do not edit below this line
+
+/**Do not edit below this line*/
 module.exports = repeatString;
+
+
