6d33468a62c8aafef1c468663b25a5aaa5d51ae4
Gui Costa <gui.costalp@gmail.com>
2022-06-11 11:56:23 +1200

    Missing test 5 to pass. Remove strings from function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,35 @@
-const sumAll = function() {
+const sumAll = function(...arg) {
+
+    addNumber = 0;
+    firstNumber = arg[0];
+    secondNumber = arg[1];
+    lowerNumber = firstNumber;
+    largerNumber = secondNumber;
+
+
+    if (isNaN(firstNumber) || isNaN(secondNumber)) {
+        return 'ERROR';
+    }
+    
+    if (firstNumber < 0 || secondNumber <0) {
+
+        return 'ERROR';
+    }
+
+        if (secondNumber < firstNumber){
+
+            lowerNumber = secondNumber;
+            largerNumber = firstNumber;
+
+
+        }    
+
+        for (let i=lowerNumber; i <= largerNumber ; i++) {
+            addNumber += i;
+        }
+
+        return addNumber;       
+
 
 };
 
