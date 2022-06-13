e92b127bdc31f2188923be1e74217af88955b17e
Gui Costa <gui.costalp@gmail.com>
2022-06-13 20:22:12 +1200

    Ex9 passed all tests. WOrking on Ex10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,24 +1,26 @@
 const palindromes = function (str) {
       
-    str = str.toLowerCase();
+    str = str.toLowerCase().replace(/[^a-z]/g, "");
     reverseStr = "";
-
-    let strLength = str.length;
+    strLength = str.length;
 
     for (let i = (strLength - 1); i >= 0 ; --i) {
 
         reverseStr += str.charAt(i);
-
+        
     }
 
-    if (str.equals(reverseStr)) {
+    for (let i = (strLength - 1); i >= 0 ; --i) {
 
-        return true
+        if (str.charAt(i) != reverseStr.charAt(i)) {
 
+            return false;
+        }
+        
     }
 
-    return false;
-
+    
+    return true;
 
 
 
