6bb33903ddc42eb3b8227dae166c999e8fc24f5c
Gui Costa <gui.costalp@gmail.com>
2022-06-16 12:41:58 +1200

    Ex13 in progress

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,4 +1,16 @@
-const caesar = function() {
+const caesar = function(phrase, index) {
+
+    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", 
+    "W", "X", "Y", "Z"];
+
+
+    findPosition = alphabet.indexOf(phrase);
+
+    newPosition = alphabet[findPosition + index];
+
+    return newPosition
+
 
 };
 
