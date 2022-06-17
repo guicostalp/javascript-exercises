0d4935007a169726fe050a8a93f805f30a1ad22c
Gui Costa <gui.costalp@gmail.com>
2022-06-18 10:43:53 +1200

    Ex13 in progress. Working on wraps

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,53 +1,76 @@
-const caesar = function(phrase, index) {
+const caesar = function(words, index) {
 
-     
-    phrase = phrase.toLowerCase();
+  
+      
+  phrase = words.toLowerCase();
 
       
-    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-                "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
-                "u", "v", "w", "x", "y", "z"];
+  alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+              "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
+              "u", "v", "w", "x", "y", "z"];
 
-    punctuation = [" ", "!", ","]                    
+  punctuation = [" ", "!", ","]                    
 
-    cipher = "";
-
-
-
-    for (let i = 0; i <= (phrase.length - 1) ; ++i) {
-
-      if (punctuation.includes(phrase.charAt(i))) {  
-
-          cipher += phrase.charAt(i);
-
-      }
-
-      else { 
-        
-        findPosition = alphabet.indexOf(phrase.charAt(i));    
-
-        cipherLetter = alphabet[findPosition + index];
-
-          if ((findPosition + index) > (alphabet.length - 1)) {
-
-            wrappedPosition = (findPosition + index) - (alphabet.length - 1);
-
-            cipherLetter = alphabet[wrappedPosition - 1];
+  cipher = "";
 
 
-          }
 
+  for (let i = 0; i <= (phrase.length - 1) ; ++i) {
 
-    
-        cipher += cipherLetter;
+    if (punctuation.includes(phrase.charAt(i))) {  
 
-      }
+        cipher += phrase.charAt(i);
 
     }
 
+    else { 
+      
+      findPosition = alphabet.indexOf(phrase.charAt(i));    
+
+      cipherLetter = alphabet[findPosition + index];
+
+        if ((findPosition + index) > (alphabet.length - 1)) {
+
+          wrappedPosition = (findPosition + index) - (alphabet.length - 1);
+
+          cipherLetter = alphabet[wrappedPosition - 1];
 
 
-    return cipher.charAt(0).toUpperCase() + cipher.slice(1);
+        }
+
+
+  
+      cipher += cipherLetter;
+
+    }
+
+  }
+
+prepCipher = cipher.charAt(0) + cipher.slice(1); 
+
+finalCipher = "";
+
+
+  
+for (let i = 0; i <= (words.length - 1) ; ++i) {      
+
+    if (words.charAt(i) == phrase.charAt(i)) {
+        finalCipher += prepCipher.charAt(i);
+
+         
+    }
+
+    else {
+
+       finalCipher += prepCipher.charAt(i).toUpperCase();
+
+
+    }
+
+}
+
+
+return finalCipher
 };
 
 // Do not edit below this line
