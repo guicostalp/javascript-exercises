80dc19e1d0451da8bc0805540a5a75a8cc325277
Gui Costa <gui.costalp@gmail.com>
2022-06-17 12:53:39 +1200

    Ex13 in progress, working on upper case letters.

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,17 +1,53 @@
 const caesar = function(phrase, index) {
 
-    alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", 
-    "W", "X", "Y", "Z"];
+     
+    phrase = phrase.toLowerCase();
+
+      
+    alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+                "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", 
+                "u", "v", "w", "x", "y", "z"];
+
+    punctuation = [" ", "!", ","]                    
+
+    cipher = "";
 
 
-    findPosition = alphabet.indexOf(phrase);
 
-    newPosition = alphabet[findPosition + index];
+    for (let i = 0; i <= (phrase.length - 1) ; ++i) {
 
-    return newPosition
+      if (punctuation.includes(phrase.charAt(i))) {  
+
+          cipher += phrase.charAt(i);
+
+      }
+
+      else { 
+        
+        findPosition = alphabet.indexOf(phrase.charAt(i));    
+
+        cipherLetter = alphabet[findPosition + index];
+
+          if ((findPosition + index) > (alphabet.length - 1)) {
+
+            wrappedPosition = (findPosition + index) - (alphabet.length - 1);
+
+            cipherLetter = alphabet[wrappedPosition - 1];
 
 
+          }
+
+
+    
+        cipher += cipherLetter;
+
+      }
+
+    }
+
+
+
+    return cipher.charAt(0).toUpperCase() + cipher.slice(1);
 };
 
 // Do not edit below this line
